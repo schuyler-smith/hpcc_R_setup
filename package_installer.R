4f1ace5081aafd600eda969e71f4340fceecce3b
sdsmith1390 <sdsmith@iastate.edu>
2018-11-15 13:51:53 -0600

    fix github install

--- package_installer.R
+++ package_installer.R
@@ -3,18 +3,19 @@
 args = commandArgs(trailingOnly=TRUE)
 
 .inst_packages <- args[2:length(args)]
-.inst <- .inst_packages %in% installed.packages()
 
 if(args[2] == "github_packages"){
-	.inst_packages <- args[3:length(args)];.inst <- .inst_packages %in% installed.packages()
-	if(any(!.inst)) {devtools::install_github(.inst_packages[!.inst])}
-	#split after / to get package name to require
+  .github_package <- args[3:length(args)]
+  .inst_packages <- unlist(lapply(sapply(.github_package, strsplit, "/"), '[[',2))
+  .inst <- .inst_packages %in% installed.packages()
+  if(any(!.inst)) {devtools::install_github(.github_package[!.inst])}
+} else if(args[2] == "bioc_packages"){
+  .inst <- .inst_packages %in% installed.packages()
+  .inst_packages <- args[3:length(args)];.inst <- .inst_packages %in% installed.packages()
+  if(any(!.inst)) {source("http://bioconductor.org/biocLite.R"); biocLite(.inst_packages[!.inst], ask = F)}
+} else {
+  .inst <- .inst_packages %in% installed.packages()
+  if(any(!.inst)) {install.packages(.inst_packages[!.inst], lib=args[1], contriburl = contrib.url('http://cran.r-project.org/'))}
 }
-if(args[2] == "bioc_packages"){
-	.inst_packages <- args[3:length(args)];.inst <- .inst_packages %in% installed.packages()
-	if(any(!.inst)) {source("http://bioconductor.org/biocLite.R"); biocLite(.inst_packages[!.inst], ask = F)}
-}
-
-if(any(!.inst)) {install.packages(.inst_packages[!.inst], lib=args[1], contriburl = contrib.url('http://cran.r-project.org/'))}
 
 sapply(.inst_packages, require, character.only = TRUE)
